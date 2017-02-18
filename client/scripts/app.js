464ee9db1b381300706cb28bdc041d00afd8030e
blhwong <b.lh.wong@gmail.com>
2017-02-18 10:58:52 -0800

    renderMessage no longer duplicates msgs

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -28,6 +28,7 @@ var app = {
       // This is the url you should use to communicate with the parse API server.
       url: url,
       type: 'GET',
+      data: 'order=-createdAt',
       // data: JSON.stringify(message),
       // contentType: 'application/json',
       success: function (data) {
@@ -42,17 +43,47 @@ var app = {
       error: function (data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
         console.error('chatterbox: Failed to receive message', data);
-      }
+      },
+      // data-urlencode: order=-score;
     });
   },
   clearMessages: function() {
     $('#chats').empty();
   },
   renderMessage: function(message) {
-    // $('#chats').append($('<div class="username">', message.text));
-    $('#chats').append($('<div class="container"></div>'));
-    $('.container').append($('<div class="username">' + message.username + ':</div>'));
-    $('.container').append($('<div class="message">' + message.text + '</div>'));
+    debugger;
+    // $('#chats').append($('<div class="container"></div>'));
+    // $('.container').append($('<div class="username">' + message.username + ':</div>'));
+    // $('.container').append($('<div class="message">' + message.text + '</div>'));
+
+    //make container div
+    var container = document.createElement('div');
+    container.className = 'container'; // <div class="container"></div>
+
+    var username = document.createElement('div');
+    username.className = 'username';
+    username.append(message.username + ':');
+    
+    var text = document.createElement('div');
+    text.className = 'text';
+    text.append(message.text);
+
+    container.append(username);
+    container.append(text);
+    $('#chats').append(container);
+    //append username into into container
+    // container.append(('<div class="username">' + message.username + ':</div>'));
+    // //append message into container
+    // container.append(('<div class="text">' + message.text + '</div>'));
+    // //append container to chat
+    // $('#chats').append(container);
+
+    //OR
+
+    //append container into chats
+    //append last instance of container
+    //append last instance of container
+
   },
   renderRoom: function(room) {
     // var roomName = message.
