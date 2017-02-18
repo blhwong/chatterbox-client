6e31b1429c56e8d41ddafe1947786be0142eef31
blhwong <b.lh.wong@gmail.com>
2017-02-17 16:56:24 -0800

    Pass renderMessage test

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,7 +3,7 @@ var app = {
   init: function() {
 
   },
-  send: function() {
+  send: function(message) {
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
       url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
@@ -19,7 +19,35 @@ var app = {
       }
     });
   },
-  fetch: function() {
+  fetch: function(url) {
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: url,
+      type: 'GET',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message received');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to receive message', data);
+      }
+    });
+  },
+  clearMessages: function() {
+    $('#chats').empty();
+  },
+  renderMessage: function(message) {
+    debugger;
+    var username = message.username;
+    var text = message.text;
+    var newDivElement = document.createElement('div');
+
+    newDivElement.append(username + ' ' + text);
+    //var $message = $('<div>', message.text);
+    console.log(newDivElement);
+    $('#chats').append(newDivElement);
 
   }
 };
